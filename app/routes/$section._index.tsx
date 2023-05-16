49376bdd5f7a2dca2b69f438d1eaffe150435bf4
Emmanuel Vazquez <maanuvazquez@gmail.com>
2023-05-16 18:12:38 -0300

    feat(ssr): add defer for mobile usage

---
+++ app/routes/$section._index.tsx
@@ -0,0 +1,17 @@
+import { useAsyncValue } from '@remix-run/react'
+
+import type { Section } from '~/models/sections'
+
+import Markdown from '~/components/Markdown/Markdown'
+import { capitalizeWord } from '~/utils/string'
+
+export default function SectionLayout() {
+  const section = useAsyncValue() as Section
+  return (
+    <>
+      <h1 className="dark:text-primary">{capitalizeWord(section.name)}</h1>
+      <hr className="mt-0" />
+      <Markdown>{section.body}</Markdown>
+    </>
+  )
+}
--- app/routes/$section.tsx
+++ app/routes/$section.tsx
@@ -1,27 +1,19 @@
-import { json } from '@remix-run/node'
-import { useLoaderData } from '@remix-run/react'
-import { notFound } from 'remix-utils'
+import { Await, Outlet, useLoaderData } from '@remix-run/react'
+import { Suspense } from 'react'
 import invariant from 'tiny-invariant'
 
 import type { LoaderArgs, V2_MetaDescriptor } from '@remix-run/node'
 import type { V2_MetaArgs } from '@remix-run/react'
 
-import Markdown from '~/components/Markdown/Markdown'
 import { getSection } from '~/models/sections'
 import { generateArticleMeta } from '~/utils/meta'
-import { capitalizeWord } from '~/utils/string'
+import { checkExists, deferIfMobile } from '~/utils/ssr.server'
 
-export async function loader({ params }: LoaderArgs) {
+export async function loader({ params, request }: LoaderArgs) {
   const slug = params.section as string
   invariant(slug, 'Expected a section slug')
 
-  const section = await getSection(slug)
-
-  if (!section) {
-    throw notFound({})
-  }
-
-  return json({ section })
+  return deferIfMobile({ section: getSection(slug).then(checkExists) }, request)
 }
 
 export const handle = {
@@ -39,15 +31,13 @@ export function meta({ data }: V2_MetaArgs): V2_MetaDescriptor[] {
 export default function PostSlug() {
   const { section } = useLoaderData<typeof loader>()
 
-  if (!section) {
-    return null
-  }
-
   return (
     <article className="max-w-screen prose mx-4 h-full sm:mx-auto">
-      <h1 className="dark:text-primary">{capitalizeWord(section.name)}</h1>
-      <hr className="mt-0" />
-      <Markdown>{section.body}</Markdown>
+      <Suspense fallback={null}>
+        <Await resolve={section}>
+          <Outlet />
+        </Await>
+      </Suspense>
     </article>
   )
 }
